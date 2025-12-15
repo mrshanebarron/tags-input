484f3b254bf2f4c34075dea55d05386136948e0d
Shane Barron <shane@Mac.hsd1.fl.comcast.net>
2025-12-15 18:58:18 -0500

    Fix PHP syntax - use PascalCase for all class names

--- src/TagsInputServiceProvider.php
+++ src/TagsInputServiceProvider.php
@@ -3,26 +3,36 @@
 namespace MrShaneBarron\TagsInput;
 
 use Illuminate\Support\ServiceProvider;
+use MrShaneBarron\TagsInput\Livewire\TagsInput;
+use MrShaneBarron\TagsInput\View\Components\TagsInput as BladeTagsInput;
 
 class TagsInputServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/tags-input.php', 'sb-tags-input');
+        $this->mergeConfigFrom(__DIR__ . '/../config/sb-tags-input.php', 'sb-tags-input');
     }
 
     public function boot(): void
     {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sb-tags-input');
+
         if (class_exists(\Livewire\Livewire::class)) {
-            \Livewire\Livewire::component('sb-tags-input', Livewire\TagsInput::class);
+            \Livewire\Livewire::component('sb-tags-input', TagsInput::class);
         }
 
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sb-tags-input');
+        $this->loadViewComponentsAs('ld', [
+            BladeTagsInput::class,
+        ]);
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/../config/tags-input.php' => config_path('sb-tags-input.php'),
+                __DIR__ . '/../config/sb-tags-input.php' => config_path('sb-tags-input.php'),
             ], 'sb-tags-input-config');
+
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/sb-tags-input'),
+            ], 'sb-tags-input-views');
         }
     }
 }
